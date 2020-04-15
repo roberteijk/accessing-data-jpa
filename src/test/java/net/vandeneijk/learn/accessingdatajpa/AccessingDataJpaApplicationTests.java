aade3e69338ab8faa2e4372199704d2d6c199cd1
Robert van den Eijk <robert@vandeneijk.net>
2020-04-15 10:43:15 +0200

    Initial commit.

---
+++ src/test/java/net/vandeneijk/learn/accessingdatajpa/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vandeneijk.learn.accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
